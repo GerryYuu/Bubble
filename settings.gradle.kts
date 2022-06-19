113d5d4c33b915b5f29c225253c6301b280f95b4
GerryYuu <gerryyuu@outlook.com>
2022-06-19 11:43:49 +0800

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "Bubble"
+
+include("bubble-api", "bubble-server")
